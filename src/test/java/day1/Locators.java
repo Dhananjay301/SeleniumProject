704d73e6737366a15135080d42a0d815c59f3c3b
admin <admin@DESKTOP-FOTJODL>
2025-05-01 14:32:28 +0530

    First selenium commit

---
+++ src/test/java/day1/Locators.java
@@ -0,0 +1,18 @@
+package day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+	public static void main(String[] args) {
+		//Launch browser
+		
+				ChromeDriver driver=new ChromeDriver();
+				
+//				open url
+				driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+				
+
+	}
+
+}
---
+++ src/test/java/webDriverMethods/GetMethods.java
@@ -0,0 +1,53 @@
+package webDriverMethods;
+
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetMethods {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver=new ChromeDriver();
+		
+//		get()
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+
+//		getTitle
+		String title=driver.getTitle();
+		System.out.println("Titile= "+title);
+		
+//		Current url
+		String currentUrl=driver.getCurrentUrl();
+		System.out.println("currentUrl = "+currentUrl);
+		
+////		getPageSource
+//		String pageSource=driver.getPageSource();
+//		System.out.println("pageSource = "+pageSource);
+		
+//		getWindowHandle
+		String windowHandle=driver.getWindowHandle();
+		System.out.println("windowHandle= "+windowHandle);
+		
+//		getWindow handles with single window
+		Set<String> windowHandles= driver.getWindowHandles();
+		System.out.println("windowHandles= "+windowHandles);
+		
+//		getWindow handles with two window
+		WebElement clickWindow=driver.findElement(By.xpath("//*[text()='OrangeHRM, Inc']"));
+		clickWindow.click();
+		Thread.sleep(5000);
+		Set<String> setWindowHandles=driver.getWindowHandles();
+		System.out.println("setWindowHandles "+setWindowHandles);
+		
+
+		
+		System.out.println("Passed");
+	}
+
+}
